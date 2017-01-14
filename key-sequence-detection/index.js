4a34f869c280c06ff4a4e393e36f4fa0dab7bb79
Greg Clinton <gregclinton1@gmail.com>
2017-01-14 23:02:44 +0000

    key-sequence-detection

---
+++ key-sequence-detection/index.js
@@ -0,0 +1,16 @@
+const pressed = [];
+
+const secretCode = 'susan';
+
+window.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.lenght - secretCode.length);
+  console.log(pressed);
+
+  if(pressed.join('').includes(secretCode)){
+    console.log('DING DING');
+    cornify_add();
+  }
+console.log(pressed);
+});
